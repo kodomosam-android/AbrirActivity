d122bec7bff6d9f8ee0983fb052b3104672d5183
kodom <sampaiorodrigomoreira@gmail.com>
2019-08-12 19:54:49 -0300

    primeiro commit

---
+++ app/src/main/java/com/casting99/abriractivity/SegundaActivity.java
@@ -0,0 +1,16 @@
+package com.casting99.abriractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.casting99.abriractivitypassarparametro.R;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
